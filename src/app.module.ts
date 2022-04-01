f94f84cbceef0d9feb1436fdfca7482a60184c55
unknown <Student@108-00.vssdomena.scv.si>
2022-04-01 12:20:21 +0200

    user modul, controller, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+      UserModule,
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'postgres',
+        database: 'node22',
+          autoLoadEntities: true,
+        entities: [ ],
+        synchronize: true,
+      }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

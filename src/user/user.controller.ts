d66665aa387903fb1568701ba311933199e4317a
unknown <Student@108-00.vssdomena.scv.si>
2022-04-08 11:45:30 +0200

    CRUD za userja

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,38 @@
-import {Controller, Get} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
+import {UserService} from "./user.service";
+import {User} from "./user.entity";
 
 @Controller('users')
 export class UserController {
+    constructor(private userService: UserService) {
+    }
 
-    @Get()
+    @Get('users')
     all() {
-        return 'VSS je zakon!';
+        return this.userService.all();
+    }
+
+    @Post('user')
+    create (@Body() data): Promise<User> {
+        return this.userService.create(data);
+    }
+
+    @Get(':id')
+    getUserById(@Param('id') id: number) : Promise<User> {
+        return this.userService.findOne({id});
+    }
+
+    @Put(':id')
+    async update(
+        @Param('id') id:number,
+        @Body() data
+    ) : Promise<User> {
+        return await this.userService.update(id,data);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id:number) : Promise<any> {
+        return this.userService.delete(id);
     }
 
 }
